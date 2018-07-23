1a360a39a3bd0e03bd9a39a78da2da4219947d27
Tomi Korkalainen <seiskytseven@gmail.com>
2018-07-24 00:41:00 +0300

    Tausta editointia
    Varjostusta... Taustaksi voisi laittaa vielä muuri seinän... Eli jostain tiilitekstuurista luoda muurin hahmon taakse.

--- main.lua
+++ main.lua
@@ -27,7 +27,7 @@ rakennukset.anchorY = 1.0
 rakennukset.x = 0
 rakennukset.y = 210
 rakennukset:scale( 0.2, 0.2 )
-rakennukset.speed = 1
+rakennukset.speed = 0
 rakennukset.leveys = 1630
 
 local rakennukset2 = display.newImage("images/city_background_clean.png")
@@ -36,7 +36,7 @@ rakennukset2.anchorY = 1.0
 rakennukset2.x = 1638
 rakennukset2.y = 210
 rakennukset2:scale( 0.2, 0.2 )
-rakennukset2.speed = 1
+rakennukset2.speed = 0
 rakennukset2.leveys = 1630
 
 
@@ -47,7 +47,7 @@ asphalt.anchorY = 0.0
 asphalt.x = 0
 asphalt.y = 220
 asphalt:scale( 0.33, 0.33 )
-asphalt.speed = 4
+asphalt.speed = 0
 asphalt.leveys = 1348
 
 
@@ -57,8 +57,30 @@ asphalt2.anchorY = 0.0
 asphalt2.x = 1352
 asphalt2.y = 220
 asphalt2:scale( 0.33, 0.33 )
-asphalt2.speed = 4
+asphalt2.speed = 0
 asphalt2.leveys = 1348
+
+
+
+
+local brick = display.newImage("images/brickk.jpg")
+brick.anchorX = 0.1
+brick.anchorY = 1.0
+brick.x = 0
+brick.y = 220
+brick.height = 10
+brick.speed = 0
+brick.leveys = 1140
+
+
+local brick2 = display.newImage("images/brickk.jpg")
+brick2.anchorX = 0.1
+brick2.anchorY = 1.0
+brick2.x = 1144
+brick2.y = 220
+brick2.height = 10
+brick2.speed = 0
+brick2.leveys = 1140
 
 
 function scrollBackground(self, event)
@@ -85,9 +107,39 @@ asphalt2.enterFrame = scrollBackground
 Runtime:addEventListener("enterFrame", asphalt2)
 
 
-local lattia = display.newRect( 0, 270, 700, 10 )
+brick.enterFrame = scrollBackground
+Runtime:addEventListener("enterFrame", brick)
+
+brick2.enterFrame = scrollBackground
+Runtime:addEventListener("enterFrame", brick2)
+
+local paint2 = {
+	type = "gradient",
+	color1 = { 0, 0, 0 },
+	color2 = { 0.4, 0.4, 0.4, 0.4},
+	direction = "up"
+}
+
+local paint3 = {
+	type = "gradient",
+	color1 = { 0, 0, 0 },
+	color2 = { 0.4, 0.4, 0.4, 0.4},
+	direction = "down"
+}
+
+
+local varjo = display.newRect( 0, 220, 1100, 60 )
+varjo.anchorX = 0.1
+varjo.anchorY = 0.0
+varjo.fill = paint2
+varjo.alpha = 0.4
+
+
+local lattia = display.newRect( 0, 280, 1100, 90 )
 lattia.anchorX = 0.1
-lattia.anchorY = 1.0
+lattia.anchorY = 0.0
+lattia.fill = paint3
+lattia.alpha = 0.1
 physics.addBody( lattia, "static", {friction=0.5, bounce=0.3 } )
 
 
